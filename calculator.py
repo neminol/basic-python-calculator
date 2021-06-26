9d2e3340dca090d8243678e9574ae62a3a8836ab
KalenMajic <84968696+KalenMajic@users.noreply.github.com>
2021-06-26 11:48:55 +0200

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,8 +1,27 @@
-# Made by Quasm Systems in fun
+# Made by Quasm Systems for fun
 # Basic calculator
 
 num1 = input("Enter your number:")
 num2 = input("Enter your number:")
+operation = print("Enter your math operation (+ - * /)")
+inputoperation = input()
 
-result = float(num1) + float(num2)
-print(result)
+plus = float(num1) + float(num2)
+minus = float(num1) - float(num2)
+multiply = float(num1) * float(num2)
+divide = float(num1) / float(num2)
+
+if inputoperation == "+":
+    print(plus)
+
+elif inputoperation == "-":
+    print(minus)
+
+elif inputoperation == "*":
+    print(multiply)
+
+elif inputoperation == "/":
+    print(divide)
+
+else:
+    print("Cannot read operation.")
